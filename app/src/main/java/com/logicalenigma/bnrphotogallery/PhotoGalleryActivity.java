c2e22cf3d8aeacfb4f214f73285df8ce7b1615bd
David Nachman <david@nachman.org>
2015-09-11 19:04:52 -0400

    Initial commit

---
+++ app/src/main/java/com/logicalenigma/bnrphotogallery/PhotoGalleryActivity.java
@@ -0,0 +1,11 @@
+package com.logicalenigma.bnrphotogallery;
+
+import android.support.v4.app.Fragment;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new PhotoGalleryFragment();
+    }
+}
---
+++ app/src/main/java/com/logicalenigma/bnrphotogallery/PhotoGalleryFragment.java
@@ -0,0 +1,38 @@
+package com.logicalenigma.bnrphotogallery;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Copyright 2015 Logical Enigma
+ * Created by dnachman on 9/11/15.
+ */
+public class PhotoGalleryFragment extends Fragment {
+
+    private RecyclerView mPhotoRecyclerView;
+
+    public static PhotoGalleryFragment newInstance() {
+        return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_photo_gallery_recycler_view);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        return v;
+    }
+}
